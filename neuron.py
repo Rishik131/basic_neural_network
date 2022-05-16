e42a277593696fbcf126b95ce8bbffbedad6969f
Rishik Kumar <rishik.k2002@gmail.com>
2022-05-16 13:59:41 +0530

    forward function created

--- neuron.py
+++ neuron.py
@@ -4,3 +4,11 @@ x = [
         [2.1,2.2,2.3],
         [3.1,3.2,3.3]
     ]
+
+class layer_dense:
+    def __init__(self,n_input,n_neuron):
+        self.weights = .1 * np.random.randn(n_input,n_neuron)
+        self.bias = np.zeros((1,n_neuron))
+
+    def forward(self,inputs):
+        self.output = np.dot(inputs,self.weights) + self.bias
